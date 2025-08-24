1c14fc5f09eff6a29f1b5aa0c06ca94bbac5230e
Nico Jersch <jersch@splid.app>
2025-08-24 15:39:56 +0200

    Split main background script into subfiles

---
+++ chrome_extension/sheet_shortcuts.js
@@ -0,0 +1,108 @@
+import * as config from './config.js';
+
+
+/** Key for sheet ID in URL's hash parameters. */
+const SHEET_HASH_PARAM_KEY = 'gid';
+
+
+/** Key for filter view ID in URL's hash parameters. */
+const FILTER_VIEW_HASH_PARAM_KEY = 'fvid';
+
+
+/**
+ * Switches to filter view if current tab is the backlog spreadsheet.
+ * @param {string} filterViewId Filter view ID to switch to.
+ * @returns {string} ID of the previous filter view, or null if filter view was not changed.
+ */
+export async function switchToFilterView(filterViewId) {
+
+  const activeTab = await getActiveTabIfBacklogSheet();
+
+  if (!activeTab) {
+    return null;
+  }
+
+  const hashParams = getHashParams(activeTab.url);
+
+  // Get previous filter view ID.
+  const previousFilterViewId = hashParams[FILTER_VIEW_HASH_PARAM_KEY];
+
+  // Set filter view in hash parameters.
+  hashParams[FILTER_VIEW_HASH_PARAM_KEY] = filterViewId;
+  const hash = Object.entries(hashParams)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
+  const url = new URL(activeTab.url);
+  url.hash = `#${hash}`;
+  await chrome.tabs.update(activeTab.id, {url: url.toString()});
+
+  return previousFilterViewId;
+}
+
+
+/**
+ * Reloads the filter view.
+ */
+export async function reloadFilterView() {
+  const previousFilterViewId = await switchToFilterView(config.ALL_FILTER_VIEW_ID);
+  if (previousFilterViewId !== null) {
+    await switchToFilterView(previousFilterViewId);
+  }
+}
+
+
+/**
+ * Marks the selected backlog item as done.
+ */
+export async function markSelectedItemAsDone() {
+  const activeTab = await getActiveTabIfBacklogSheet();
+  if (!activeTab) {
+    return;
+  }
+  const completedColumn = String.fromCharCode(64 + config.COMPLETED_COLUMN);
+  const message = { action: 'mark_item_done', completedColumn: completedColumn };
+  chrome.tabs.sendMessage(activeTab.id, message);
+}
+
+
+/**
+ * Returns hash parameters from a URL. Hash parameters are key-value pairs separated by ampersands,
+ * e.g. #gid=0&fvid=123.
+ * @param {string} url URL
+ * @returns {object} Hash parameters, e.g. { gid: '0', fvid: '123' }
+ */
+function getHashParams(url) {
+  const hash = new URL(url).hash;
+  const hashParams = {};
+  if (hash.startsWith('#')) {
+    const params = hash.substring(1).split('&');
+    for (const param of params) {
+      const [key, value] = param.split('=');
+      hashParams[key] = value;
+    }
+  }
+  return hashParams;
+}
+
+
+/**
+ * Returns the active tab if it is a Google Sheets page and the current sheet is the backlog sheet.
+ * @returns {object} Active tab if it is a Google Sheets page and the current sheet is the backlog sheet, otherwise null.
+ */
+async function getActiveTabIfBacklogSheet() {
+  
+  const [activeTab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+  // Check if current tab is a Google Sheets page.
+  if (!activeTab || !activeTab.url.includes('docs.google.com/spreadsheets')) {
+    return null;
+  }
+
+  // Check if current sheet is the backlog sheet.
+  const hashParams = getHashParams(activeTab.url);
+  if (hashParams[SHEET_HASH_PARAM_KEY] !== config.BACKLOG_SHEET_ID.toString()) {
+    return null;
+  }
+
+  return activeTab;
+}
---
+++ chrome_extension/tab_shortcuts.js
@@ -0,0 +1,70 @@
+import * as config from './config.js';
+
+
+export function switchToSpreadsheet() {
+  switchToTab(
+    `https://docs.google.com/*/${config.SPREADSHEET_ID}*`,
+    `https://docs.google.com/spreadsheets/d/${config.SPREADSHEET_ID}/edit`
+  );
+}
+
+export function switchToInbox() {
+  switchToTab(
+    'https://mail.google.com/*',
+    'https://mail.google.com/mail/u/0'
+  );
+}
+
+export function switchToCalendar() {
+  switchToTab(
+    'https://calendar.google.com/*',
+    'https://calendar.google.com/calendar/u/0'
+  );
+}
+
+
+/**
+ * Switches to or opens a tab matching a provided URL
+ * @param urlPattern Pattern to match open tabs against
+ * @param url URL to open if no corresponding tab is found
+ */
+async function switchToTab(urlPattern, url) {
+
+  const tabs = await chrome.tabs.query({ url: urlPattern });
+  let tab = tabs.length > 0 ? tabs[0] : null;
+
+  // Open tab if needed
+  if (tab == null) {
+
+    // Open window if needed
+    const windows = await chrome.windows.getAll({ windowTypes: ['normal'] });
+    let tabsToClose = null;
+    if (windows.length == 0) {
+      const window = await chrome.windows.create({ focused: true });
+      tabsToClose = window.tabs;
+    }
+
+    // Open tab
+    tab = await chrome.tabs.create({
+      url: url,
+      index: 0,
+      pinned: true,
+      active: true
+    });
+
+    // Close other tabs of new window if needed
+    if (tabsToClose) {
+      await chrome.tabs.remove(tabsToClose.map((tab) => tab.id));
+    }
+  } else {
+
+    // Switch to tab
+    await chrome.tabs.highlight({
+      windowId: tab.windowId,
+      tabs: tab.index
+    });
+  }
+
+  // Focus window
+  await chrome.windows.update(tab.windowId, { focused: true });
+}
