9d0a9b9026ac1569ad319272673c95b53be0448c
Nico Jersch <njersch@users.noreply.github.com>
2025-12-23 14:16:28 +0100

    Document configurable shortcuts in local config file

--- chrome_extension/config.local.js
+++ chrome_extension/config.local.js
@@ -4,4 +4,25 @@ export default {
   
   // Deployment URL of script to which a POST request can be made to schedule items.
   WEB_APP_DEPLOYMENT_URL: '',
+  
+  // Actions for configurable keyboards shortcuts.
+  // {
+  //   // Actions for "Configurable shortcut 1"
+  //   '1': [
+  //     
+  //     // First action
+  //     {
+  //       'url_pattern': 'https://example.com/*', // URL pattern as defined in https://developer.chrome.com/docs/extensions/develop/concepts/match-patterns
+  //       'current_tab_only': true, // whether to perform action only if current tab matches URL pattern above
+  //       'execute_command': 'my-command', // name of command to execute, as declared under "commands" in manifest
+  //       'switch_to_tab': 'https://example.com', // whether to switch to tab matching URL pattern above or open a new tab with the provided URL
+  //       'change_url': 'https://example.com/foo#bar', // URL to set in last accessed tab that matches URL pattern above
+  //     },
+  //     // Second action
+  //     { /* ... */ }
+  //
+  //   // Actions for "Configurable shortcut 2"
+  //   '2': { /* ... */ }
+  // }
+  CONFIGURABLE_SHORTCUTS: {}
 }
