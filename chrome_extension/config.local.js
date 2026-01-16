c55c2770e7c97321718c7f8dbbff9037b3b6d426
Nico Jersch <njersch@users.noreply.github.com>
2026-01-16 16:12:09 +0100

    Update config.local.js
    Update example usage for code execution from configurable shortcuts

--- chrome_extension/config.local.js
+++ chrome_extension/config.local.js
@@ -17,6 +17,7 @@ export default {
   //       'url_filter': 'https://example.com/*', // regex to filter tabs based on URLs
   //       'current_tab_only': true, // whether to perform action only if current tab matches URL filter above
   //       'execute_command': 'my-command', // name of command to execute, as declared under "commands" in manifest
+  //       'execute_code: (tabId) => {…}, // function to execute, taking as argument the ID of the tab matching the URL filter above
   //       'switch_to_tab': 'https://example.com', // whether to switch to tab matching URL filter above or open a new tab with the provided URL
   //       'change_url': 'https://example.com/foo#bar', // URL to set in last accessed tab that matches URL filter above
   //     },
